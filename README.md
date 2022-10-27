ae659115a1d12610c0a3560b32813759fa6de8ba
realtouseef <touseef69r@gmail.com>
2022-10-27 17:08:03 +0500

    chore: emotionBasedVerses

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ An API that shows verses from the Qur'an based on emotions.
 ## Endpoint
 
 1. `/verses` returns all of the verses that are present
-1. `/verses/:id` returns a verse based on the id.
+1. `/verses/:id` returns a verse based on the `id`.
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const versesRoutes = require("./routes/verses");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use("/verses", versesRoutes);
